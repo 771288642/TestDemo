255221712b5f2257565559369b5008993ec994f9
771288642 <analysysmac-1@analysysmac-1deiMac.local>
2018-07-19 15:26:04 +0800

    first commit

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by analysysmac-1 on 2018/7/19.
+//  Copyright © 2018年 analysysmac-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by analysysmac-1 on 2018/7/19.
+//  Copyright © 2018年 analysysmac-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
